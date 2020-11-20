e68d8c1a32d7c5d2edb06f18a57044f0877fa9a0
Reggie Littleton <Hitsupod@gmail.com>
2020-11-19 21:42:50 -0600

    Initialization

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":" https://hitsupod.github.io/Min_Portfolio/",
+    "name": "Reggie Littleton",
+    "role": "Frontend Developer and Graphic Designer",
+    "linkedinId":"reginald-littleton-b9803185",
+    "skypeid": "Hitsupod",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, freelance or stream video games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/reginald-littleton-b9803185/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Hitsupod",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/hitsupod",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a Business Administration graduate of The University of Texas at San Antonio as well as certified full stack developer with an interest in front end development. I am looking to help businesses build responsive full scale websites while optimizing user-focused for customers with various business needs.",
+    "address":"US",
+    "website":" https://hitsupod.github.io/Min_Portfolio/",
+    "education":[
+      {
+        "UniversityName":"The University of Texas at San Antonio",
+        "specialization":"React",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
